0add119ed611be20ff79ae59e84079168b987e2a
Joshua Abu <joshujah007@gmail.com>
2025-04-22 18:34:07 +0400

    seed db with user info

--- db/seed.ts
+++ db/seed.ts
@@ -2,12 +2,19 @@ import { prisma } from "./prisma";
 import sampleData from "./sample-data";
 
 async function main() {
-	const { products } = sampleData;
+	const { products, users } = sampleData;
 
 	await prisma.product.deleteMany();
+	await prisma.account.deleteMany();
+	await prisma.session.deleteMany();
+	await prisma.verificationToken.deleteMany();
+	await prisma.user.deleteMany();
 
 	await prisma.product.createMany({
 		data: products,
+	});
+	await prisma.user.createMany({
+		data: users,
 	});
 
 	console.log("Sample data seeded successfully!");
