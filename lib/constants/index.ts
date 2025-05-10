4d1964671ad8542d804917800bb3a714413ef5f9
Joshua Abu <joshujah007@gmail.com>
2025-05-10 16:24:25 +0400

    change payment methods

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -17,7 +17,7 @@ export const shippingAddressDefaultValues = {
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
 	? process.env.PAYMENT_METHODS.split(", ")
-	: ["PayPal", "Credit Card", "Cash on Delivery"];
+	: ["PayPal", "Stripe", "CashOnDelivery"];
 
 export const DEFAULT_PAYMENT_METHOD =
 	process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
